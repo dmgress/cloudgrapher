7db0de00990caa233c1badcc28d71354f1757296
Daniel Gressmann <dmgressmann@gmail.com>
2015-10-06 01:50:32 +0200

    Replaced Ace with CodeMirror and made graph the big part of design

--- app/scripts/edit.js
+++ app/scripts/edit.js
@@ -2,66 +2,47 @@
 /* global ace, cytoscape, saveAs, require */
 
 (function(){'use strict';
-  var editor;
   var collector = require('./collectdata');
+  var myCodeMirror = CodeMirror(document.getElementById("editor"), {
+    value: "",
+    lineNumbers: true,
+    mode: "application/json",
+    foldGutter: true,
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter", "CodeMirror-lint-markers"],
+    lint: {
+      onUpdateLinting: function (annotations) {
+        if (!annotations || annotations.length == 0) {
+          showCyGraph( collector.collectCyData(JSON.parse (myCodeMirror.getDoc().getValue() )));
+        }
+      }
+    }
+  });
+  myCodeMirror.setSize('100%','800px');
   var graph;
   var graphPane;
   var graphStyleP = $.ajax({ url: 'styles/main.cycss', type: 'GET', dataType: 'text' });
-  editor = ace.edit('jsoneditor');
-  editor.setTheme('ace/theme/tomorrow_night_eighties');
-  editor.getSession().setMode('ace/mode/json');
-  editor.setOption('maxLines', 60);
-  editor.setOption('minLines', 40);
-  editor.getSession().on('changeAnnotation', function() {
-    var annotations = editor.getSession().getAnnotations();
-    $('#statusbar').toggleClass('failure', annotations.length !== 0);
-    if (annotations.length === 0) {
-      var message = editor.getSession().getLength() < 2 && editor.getSession().getLine(0).length === 0 ? 'No content' : 'Valid';
-      $('div.status-message').text(message);
-    }
-    else {
-      var firstAnnotation = annotations[0];
-      $('div.status-message').text(firstAnnotation.text + ' at '+ firstAnnotation.row + ':'+ firstAnnotation.column);
-    }
-  });
-  var StatusBar = ace.require('ace/ext/statusbar').StatusBar;
-  // create a simple selection status indicator
-  new StatusBar(editor, document.getElementById('statusbar'));
-  editor.commands.addCommand({
-    name: 'showKeyboardShortcuts',
-    bindKey: {win: 'Ctrl-Alt-h', mac: 'Command-Alt-h'},
-    exec: function(editor) {
-      ace.config.loadModule('ace/ext/keybinding_menu', function(module) {
-        module.init(editor);
-        editor.showKeyboardShortcuts();
-      });
-    }
-  });
+  var isResizing = false,
+  lastDownX = 0;
 
   var loadTemplate = function(file){
     var reader = new FileReader();
     reader.onload = function () {
       var data = reader.result;
+      console.log(data);
       if(data) {
-        editor.setValue(data);
-        editor.navigateTo(0, 0);
+        try {
+          myCodeMirror.getDoc().setValue(data);
+          showCyGraph( collector.collectCyData( JSON.parse(data) ) );
+          $('#graph_area').css('background-image','');
+        }
+        catch (e) {
+          console.log('ERR - ' + e );
+        }
       }
-      editor.resize();
     };
     reader.readAsText(file);
   };
-  var showCyGraph = function() {
-    var data;
-    try {
-      data = collector.collectCyData(JSON.parse(editor.getValue()));
-    }
-    catch (e) {
-      data = {};
-    }
-    console.log(data);
-    if (!graphPane) {
-      graphPane = $('div.graph_overlay');
-    }
+  var showCyGraph = function(data) {
     graph = cytoscape({
       container: document.getElementById('graph_area'),
       elements: data,
@@ -71,7 +52,7 @@
         padding: 5
       }
     });
-    graphPane.fadeIn(300);
+    graph.boxSelectionEnabled(true);
   };
   var getTemplateDescription = function() {
     var description = 'template';
@@ -82,8 +63,9 @@
   };
   var saveImage = function() {
     var saveWindow = window.open('savegraph.html');
+    graph.center(); graph.fit();
     saveWindow.onload = function() {
-      saveWindow.document.getElementById('graphPNG').src = graph.png();
+      saveWindow.document.getElementById('graphPNG').src = graph.png({full: false});
     };
   };
   var saveTemplate = function() {
@@ -91,7 +73,7 @@
     var blob = new Blob([prettyDoc], {type: 'text/plain;charset=utf-8'});
     saveAs(blob, getTemplateDescription() + '.json');
   };
-  var mainRow = document.getElementById('cfeditor');
+  var mainRow = document.getElementById('graph_area');
   mainRow.addEventListener('dragover', function(evt) {
     evt.stopPropagation();
     evt.preventDefault();
@@ -103,10 +85,37 @@
 
     loadTemplate(evt.dataTransfer.files[0]);
   }, false);
+  $('#graph_area').css('background-image',"url('/images/aws-cloudformation-template.svg')");
   $('#open_template').change(function(event){ loadTemplate(event.target.files[0]); });
   $('#save_template').click(function(event){ event.preventDefault(); saveTemplate(); return false;});
   $('#save_graph').click(function(event){ event.preventDefault(); saveImage(); return false;});
   $('#show_graph').click(function(event){ event.preventDefault(); showCyGraph(); return false;});
   $('#close_graph').click(function(event){ event.preventDefault(); graphPane.fadeOut(500); return false;});
   $('#graph_layout').change(function() { graph.layout( { 'name': $('#graph_layout').val() }); });
+  var container = $('#container'),
+  left = $('#graph_area'),
+    right = $('#editor_pane'),
+    handle = $('#border');
+
+  handle.on('mousedown', function (e) {
+    isResizing = true;
+    lastDownX = e.clientX;
+  });
+
+  $(document).on('mousemove', function (e) {
+    // we don't want to do anything if we aren't resizing.
+    if (!isResizing)
+      return;
+
+    var offsetRight = container.width() - (e.clientX - container.offset().left);
+
+    left.css('right', offsetRight);
+    right.css('width', offsetRight);
+  }).on('mouseup', function (e) {
+    if (isResizing && graph) {
+      graph.fit();
+    }
+    // stop resizing
+    isResizing = false;
+  });
 })();
