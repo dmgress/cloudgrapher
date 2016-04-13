da72847c913c7f64315d9ecd9d4df4d2c3cdb951
Daniel Gressmann <dmgressmann@gmail.com>
2016-04-13 19:32:32 +0000

    Remove unused code and specs

--- app/scripts/collectdata.js
+++ app/scripts/collectdata.js
@@ -1,53 +1,7 @@
 /* jshint devel:true */
 /* global exports, require */
+
 var findEdges = require('./findedges').findEdges;
-exports.graphOptions = {
-  'nodes': {
-    'brokenImage': 'images/unknown.png'
-  },
-  'edges': {
-    'style': 'arrow',
-    'color.highlight': 'red'
-  },
-  'stabilize': true,
-  'zoomExtentOnStabilize': true
-};
-exports.collectData = function(json) {
-  'use strict';
-  var data = {
-    nodes: [],
-    edges: []
-  };
-  var knownResources = [];
-  var possibleEdges = [];
-  var addEdge = function(edge) {
-    possibleEdges.push({
-      'from': this,
-      'to': edge.toResource,
-      'title': edge.label
-    });
-  };
-
-  for (var resourceKey in json.Resources) {
-    var resource = json.Resources[resourceKey];
-    var props = resource.Properties;
-    var group = resource.Type.toLowerCase().replace(/::/g, '-');
-    knownResources.push(resourceKey);
-    data.nodes.push({
-      'id': resourceKey,
-      'label': resourceKey,
-      'group': group,
-      'shape': 'image',
-      'image': 'images/' + group + '.png'
-    });
-    findEdges(props).forEach(addEdge, resourceKey);
-  }
-  data.edges = possibleEdges.filter(function(edge) {
-    return edge && knownResources.indexOf(edge.to) >= 0;
-  });
-  return data;
-};
-
 
 exports.collectCyData = function(json) {
   'use strict';
